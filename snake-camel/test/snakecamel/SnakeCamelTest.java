0410c2ec95596fdcea9056e1d7c20474e959c852
flowereina <g12905fr@gm.tsuda.ac.jp>
2014-05-08 23:55:40 +0900

    課題4-2です。エラーを修正しました。

--- snake-camel/test/snakecamel/SnakeCamelTest.java
+++ snake-camel/test/snakecamel/SnakeCamelTest.java
@@ -9,7 +9,7 @@ public class SnakeCamelTest {
 	@Test
 	public void snakeToCamelcaseのテスト() {
 		SnakeCamelUtil snakecamel = new SnakeCamelUtil();
-		String expected = ("Snake");
+		String expected = ("SnakeCase");
 		String actual = snakecamel.snakeToCamelcase("snake_case");
 		assertThat(actual,is(expected));
 	}
@@ -17,8 +17,24 @@ public class SnakeCamelTest {
 	@Test
 	public void camelToSnakecaseのテスト() {
 		SnakeCamelUtil snakecamel = new SnakeCamelUtil();
-		String expected = ("camelcase");
-		String actual = snakecamel.camelToSnakecase("camelcase");
+		String expected = ("camel_case");
+		String actual = snakecamel.camelToSnakecase("CamelCase");
+		assertThat(actual,is(expected));
+	}
+	
+	@Test
+	public void capitalizeのテスト() {
+		SnakeCamelUtil snakecamel = new SnakeCamelUtil();
+		String expected = ("Snake");
+		String actual = snakecamel.capitalize("snake");
+		assertThat(actual,is(expected));
+	}
+	
+	@Test
+	public void uncapitalizeのテスト() {
+		SnakeCamelUtil snakecamel = new SnakeCamelUtil();
+		String expected = ("camel");
+		String actual = snakecamel.uncapitalize("Camel");
 		assertThat(actual,is(expected));
 	}
 
